24cc3c6b9445c154270d217cbc6513d6777dd762
“Bill <igottalentco@gmail.com>
2025-04-19 21:32:29 +1000

    Initial project setup with gitignore, Flutter config and dependencies

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/billakkas/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/billakkas/Documents/Projects/forks/ffmpeg_kit_flutter_min_fork"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=6.0.4"
+export "FLUTTER_BUILD_NUMBER=6.0.4"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
