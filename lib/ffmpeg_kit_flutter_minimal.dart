f962da4b0bb1c7a877be547c0772db6a9759182e
“Bill <igottalentco@gmail.com>
2025-04-19 22:05:10 +1000

    Rename package to ffmpeg_kit_flutter_minimal for pub.dev publishing

---
+++ lib/ffmpeg_kit_flutter_minimal.dart
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2018-2022 Taner Sener
+ *
+ * This file is part of FFmpegKit.
+ *
+ * FFmpegKit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FFmpegKit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with FFmpegKit.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+library ffmpeg_kit_flutter_minimal;
+
+export 'abstract_session.dart';
+export 'arch_detect.dart';
+export 'chapter.dart';
+export 'ffmpeg_kit.dart';
+export 'ffmpeg_kit_config.dart';
+export 'ffmpeg_session.dart';
+export 'ffmpeg_session_complete_callback.dart';
+export 'ffprobe_kit.dart';
+export 'ffprobe_session.dart';
+export 'ffprobe_session_complete_callback.dart';
+export 'level.dart';
+export 'log.dart';
+export 'log_callback.dart';
+export 'log_redirection_strategy.dart';
+export 'media_information.dart';
+export 'media_information_json_parser.dart';
+export 'media_information_session.dart';
+export 'media_information_session_complete_callback.dart';
+export 'packages.dart';
+export 'return_code.dart';
+export 'session.dart';
+export 'session_state.dart';
+export 'signal.dart';
+export 'statistics.dart';
+export 'statistics_callback.dart';
+export 'stream_information.dart';
